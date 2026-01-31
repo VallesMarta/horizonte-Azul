305762fc68ebc2a05ec9e9736e9dfd06fd9dddee
Marta Valles <mvallesterol12@gmail.com>
2026-01-31 17:15:36 +0100

    Actualizamos el script .sql para añadir y ajustar campos en varias tablas y renombramos columnas para mejorar la referencia (refs #5)

--- docker/init.sql
+++ docker/init.sql
@@ -16,11 +16,13 @@ CREATE TABLE IF NOT EXISTS usuarios (
 -- ============================
 CREATE TABLE IF NOT EXISTS viajes (
     id int AUTO_INCREMENT PRIMARY KEY,
-    origen varchar(100) NOT NULL,
-    origenAeropuerto varchar(50) NOT NULL,
-    destino varchar(100) NOT NULL,
-    destinoAeropuerto varchar(50) NOT NULL,
-    precio decimal(10,2) NOT NULL DEFAULT 0,
+    paisOrigen varchar(100) NOT NULL,            
+    aeropuertoOrigen varchar(100) NOT NULL,
+    horaSalida TIME DEFAULT '00:00:00',                    
+    paisDestino varchar(100) NOT NULL,           
+    aeropuertoDestino varchar(100) NOT NULL,
+    horaLlegada TIME DEFAULT '00:00:00',                   
+    precio decimal(10,2) NOT NULL DEFAULT 0.00, 
     img varchar(255),
     descripcion text,
     created_at timestamp DEFAULT CURRENT_TIMESTAMP
@@ -31,7 +33,8 @@ CREATE TABLE IF NOT EXISTS viajes (
 -- ============================
 CREATE TABLE IF NOT EXISTS servicios (
     id int AUTO_INCREMENT PRIMARY KEY,
-    nombre varchar(100) UNIQUE
+    nombre varchar(100) UNIQUE, 
+    tipo_control ENUM('numero', 'texto', 'booleano') NOT NULL DEFAULT 'texto'
 );
 
 -- ============================
@@ -40,6 +43,8 @@ CREATE TABLE IF NOT EXISTS servicios (
 CREATE TABLE IF NOT EXISTS viaje_servicio (
     viaje_id int,
     servicio_id int,
+    valor varchar(255), 
+    precio_extra decimal(10,2) DEFAULT 0.00, 
     PRIMARY KEY (viaje_id, servicio_id),
     FOREIGN KEY (viaje_id) REFERENCES viajes(id) ON DELETE CASCADE,
     FOREIGN KEY (servicio_id) REFERENCES servicios(id) ON DELETE CASCADE
@@ -55,7 +60,7 @@ CREATE TABLE IF NOT EXISTS reservas (
     fecCompra timestamp DEFAULT CURRENT_TIMESTAMP,
     fecSalida DATE,
     pasajeros int DEFAULT 1 CHECK (pasajeros >= 1),
-    estado ENUM('pendiente','confirmada','cancelada') DEFAULT 'pendiente',
+    estado ENUM('pendiente','confirmada','realizada','cancelada') DEFAULT 'pendiente',
     FOREIGN KEY (usuario_id) REFERENCES usuarios(id) ON DELETE CASCADE,
     FOREIGN KEY (viaje_id) REFERENCES viajes(id) ON DELETE CASCADE
 );
