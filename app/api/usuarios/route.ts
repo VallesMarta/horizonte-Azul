ca0c4cf962a89df8f8c664956b021024fb55cccb
Marta Valles <mvallesterol12@gmail.com>
2026-02-17 19:46:12 +0100

    Actualizamos la ruta para mostrar correctamente el contador en el panel de administración y saber cuántas reservas tiene un usuario.

--- app/api/usuarios/route.ts
+++ app/api/usuarios/route.ts
@@ -3,9 +3,29 @@ import { query } from "@/config/db.config";
 
 export async function GET() {
   try {
-    const usuarios = await query('SELECT id, username, nombre, email, isAdmin FROM usuarios');
-    return NextResponse.json({ ok: true, resultado: usuarios });
+    const sql = `
+        SELECT 
+            id, 
+            username, 
+            nombre, 
+            email, 
+            isAdmin,
+            (SELECT COUNT(*) FROM reservas WHERE usuario_id = usuarios.id) AS total_reservas
+        FROM usuarios
+    `;
+
+    const usuarios = await query(sql);
+
+    return NextResponse.json({ 
+      ok: true, 
+      resultado: usuarios 
+    });
+    
   } catch (err) {
-    return NextResponse.json({ ok: false, error: "Error al obtener" }, { status: 500 });
+    console.error("Error al obtener usuarios:", err);
+    return NextResponse.json({ 
+      ok: false, 
+      error: "Error al obtener la lista de usuarios" 
+    }, { status: 500 });
   }
 }
