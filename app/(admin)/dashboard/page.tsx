d115ef9d1d50f011d2cdf7903bb48f888dd128fd
Marta Valles <mvallesterol12@gmail.com>
2026-02-21 21:58:51 +0100

    Añadimos un título a la página del dashboard para evitar que parezca que no funciona. Ref. (#10)

--- app/(admin)/dashboard/page.tsx
+++ app/(admin)/dashboard/page.tsx
@@ -1,6 +1,13 @@
-export default function DashboardLayout({ children }: { children: React.ReactNode }) {
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <>
+      <h1 className="text-3xl font-black text-titulo-resaltado uppercase tracking-tighter mb-8">
+        Bienvenido al panel del administrador!!!
+      </h1>
     </>
   );
 }
